a49e2f9a8d46eb3cdb3c7f704452bd92e038aeb9
AbdullahGumi <abdulgumi77@gmail.com>
2022-05-29 22:38:45 +0100

    Add uploaded video details

--- components/UploadVideoModal.tsx
+++ components/UploadVideoModal.tsx
@@ -7,6 +7,9 @@ import { useDropzone } from "react-dropzone";
 
 const UploadVideoModal = ({ isModalOpen, setModalOpen }) => {
   const [file, setFile] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
@@ -28,25 +31,33 @@ const UploadVideoModal = ({ isModalOpen, setModalOpen }) => {
         className="outline-none rounded-md flex flex-col gap-4 bg-slate-800 w-2/5 top-1/2 left-1/2 absolute "
       >
         <div className="flex flex-row justify-between pt-6 px-6 items-center">
-          <h2 className="font-medium text-xl text-white">Upload video</h2>
+          <h2 className="font-medium text-xl text-white">
+            {file ? "Add details" : "Upload video"}
+          </h2>
           <CloseIcon
             className="hover:cursor-pointer  text-white"
             onClick={() => setModalOpen(false)}
           />
         </div>
         <hr />
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
-          <div className="flex flex-col items-center gap-4 p-6">
-            <div className="bg-slate-900 w-36 h-36 rounded-full flex items-center justify-center hover:cursor-pointer">
-              <FileUploadIcon sx={{ fontSize: "65px", color: "#aaa" }} />
+        {!file ? (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <div className="flex flex-col items-center gap-4 p-6">
+              <div className="bg-slate-900 w-36 h-36 rounded-full flex items-center justify-center hover:cursor-pointer">
+                <FileUploadIcon sx={{ fontSize: "65px", color: "#aaa" }} />
+              </div>
+              <span className="text-base text-white font-medium">
+                Drag and drop video files to upload
+              </span>
             </div>
-            <span className="text-base text-white font-medium">
-              Drag and drop video files to upload
-            </span>
-            <img src={file} alt="image" />
           </div>
-        </div>
+        ) : (
+          <div className="flex flex-col ">
+            <h2 className="text-white font-semibold text-2xl">Details</h2>
+            <textarea className="bg-slate-700"></textarea>
+          </div>
+        )}
       </Box>
     </Modal>
   );
